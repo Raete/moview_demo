94366d86597098a415ea223f59bc1096dbe6af4a
reate <kattovera00@gmail.com>
2018-04-08 17:45:46 +0200

    add vuetify

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import Routes from './routes'
+import Vuetify from 'vuetify'
+ 
+
 
 // Use packages
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 // Register routes
 const router = new VueRouter({
